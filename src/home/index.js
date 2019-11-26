723c6f36106a07129cf572199cac6d5ae0401bd4
Rafael barreto <rafael.barreto@cwi.com.br>
2019-11-26 15:30:01 -0300

    update : added searchBar in home page

--- src/home/index.js
+++ src/home/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import SearchBar from 'react-native-material-design-searchbar';
 
 // import { Container } from './styles';
 
 export default function Home() {
   return (
     <View>
-      <Text> home </Text>
+      <SearchBar
+        height={50}
+        placeholder={'Digite o nome do livro ...'}
+        autoCorrect={false}
+        padding={5}
+      />
     </View>
   );
 }
