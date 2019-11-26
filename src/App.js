eeb66dc6c8687d6a34ca3cf75c3fc7c3c769f915
Rafael barreto <rafael.barreto@cwi.com.br>
2019-11-26 15:27:28 -0300

    update : added safeAreaView

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {SafeAreaView} from 'react-native';
 
 import Routes from './routes';
 
 export default function App() {
   return (
-    // <View>
-    //   <Text>oi meu chapa</Text>
-    // </View>
-    <Routes />
+    <SafeAreaView style={{flex: 1}}>
+      <Routes />
+    </SafeAreaView>
   );
 }
