467396b471290cb27f545ea3872abf315b99d4cb
Rafael barreto <rafael.barreto@cwi.com.br>
2019-11-26 15:30:45 -0300

    update : updated navbar style

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
 import Icons from 'react-native-vector-icons/Ionicons';
@@ -6,10 +7,27 @@ import Icons from 'react-native-vector-icons/Ionicons';
 import Home from './home';
 import Favoritos from './favoritos';
 
-const getTabIcon = (focused, icon, text) => (
-  <Icons name={icon} size={24}>
-    {console.log(focused ? text + 'foi' : 'nao  foi')}
-  </Icons>
+const {height, width} = Dimensions.get('window');
+const barHeight = 70;
+
+const styles = StyleSheet.create({
+  iconContent: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 15,
+  },
+  text: color => ({
+    width: width * 0.5,
+    textAlign: 'center',
+    color,
+  }),
+});
+
+const getTabIcon = (icon, color, name, focus) => (
+  <View style={styles.iconContent}>
+    <Icons name={icon} size={24} color={color} />
+    {focus && <Text style={styles.text(color)}>{name}</Text>}
+  </View>
 );
 
 const Routes = createAppContainer(
@@ -18,19 +36,30 @@ const Routes = createAppContainer(
       HOME: {
         screen: Home,
         navigationOptions: {
-          title: 'Search',
-          tabBarIcon: ({focused}) => getTabIcon(focused, 'ios-home', 'home'),
+          tabBarIcon: ({focused, tintColor}) =>
+            getTabIcon('ios-home', tintColor, 'Home', focused),
         },
       },
       FAVORITOS: {
         screen: Favoritos,
         navigationOptions: {
-          tabBarIcon: ({focused}) => getTabIcon(focused, 'ios-star-half'),
+          tabBarIcon: ({focused, tintColor}) =>
+            getTabIcon('ios-star-half', tintColor, 'Favoritos', focused),
         },
       },
     },
     {
-      barStyle: {backgroundColor: '#BDBDBD'},
+      barStyle: {
+        backgroundColor: '#EEEEEE',
+        height: barHeight,
+        justifyContent: 'flex-end',
+        padding: 0,
+        width: '100%',
+      },
+      keyboardHidesNavigationBar: true,
+      sceneAnimationEnabled: true,
+      labeled: false,
+      activeColor: '#00695C',
     },
   ),
 );
